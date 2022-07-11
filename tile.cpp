b384b2e22b03a1fe6f5d17e555d8955f576aa8dc
KrystianKiznar <krystian.kiznar@gmail.com>
2022-07-11 13:49:37 +0200

    class tile

--- tile.cpp
+++ tile.cpp
@@ -1,1 +1,35 @@
 #include "tile.h"
+
+int tile::getMinesNearby()
+{
+	return minesNearby;
+}
+
+int tile::setMinesNearby(int amount)
+{
+	minesNearby = amount;
+	return minesNearby;
+}
+
+int tile::getTileType()
+{
+	return tileType;
+}
+
+int tile::setTileType(int isBomb)
+{
+	tileType = isBomb;
+	return tileType;
+}
+
+int tile::getStatus()
+{
+	return status;
+}
+
+int tile::setStatus(int choosenStatus)
+{
+	status = choosenStatus;
+	return status;
+}
+
--- tile.h
+++ tile.h
@@ -6,9 +6,10 @@ private:
 	int tileType;
 	int status; //zakryta, odkryta, flaga
 public:
-	// getter minesNearby
-	// setter -.-
-	// getter tileType
-	// setTileType
+	int getMinesNearby();
+	int setMinesNearby(int amount);
+	int getTileType();
+	int setTileType(int isBomb);
+	int getStatus();
+	int setStatus(int s);// 0 - zakryta, 1 - odkryta, 9 - flaga
 };
-
