c40a4259f726d8c82fb9770edbbc98032d6fb9ac
vampiregrin <gabriela.zuber.11@gmail.com>
2022-07-12 11:22:48 +0200

    displayMenu method + some changes

--- menu.h
+++ menu.h
@@ -1,14 +1,20 @@
 #pragma once
+#include <iostream>
+#include <windows.h>
+
 class menu
 {
 public:
 	int choice, levelchoice;
 	void welcomeSign();
 	void mainMenu();
-	void getChoice();
+	int getChoice(int displaySelector);
 	void menuChoice(int choice);
 	void levels();
 	void levelChoosing(int levelchoice);
 	void aboutTheGame();
-};
+	void displayMenu();
 
+private:
+	int minesQuant, boardSize; //=mines quantity, size of a board's side
+};
