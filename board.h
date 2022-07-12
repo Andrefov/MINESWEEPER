e484ffff54fa02409d9746b8b7695a4e6909ebe1
Malgorzata Proc <malgorzata.proc@motorolasolutions.com>
2022-07-12 10:37:56 +0200

    new branch

--- board.h
+++ board.h
@@ -18,7 +18,7 @@ private:
 	
 public:
 	void displayBoard();
-	//konstruktor kopiuj¹cy
+	//konstruktor kopiujacy
 	Board(int y, int x, int size, int minesNumber);
 	Tile* getTile(int y, int x);
 	void setTilesAround();
